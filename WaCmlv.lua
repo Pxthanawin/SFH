88da9bd7db7571ff10a59fcf2c68bd73ca85349b
Pxthanawin <96330997+Pxthanawin@users.noreply.github.com>
2024-11-26 16:48:13 +0700

    Create WaCmlv.lua

---
+++ WaCmlv.lua
@@ -0,0 +1,31 @@
+-- Config
+getgenv().wait_time = 300
+getgenv().white_screen = true
+
+-- Script
+if getgenv().ScriptRunning then return end
+getgenv().ScriptRunning = true
+
+repeat wait() until game:IsLoaded()
+
+-- White Screen
+if white_screen then
+	local RunService = game:GetService("RunService")
+	RunService:Set3dRenderingEnabled(false)
+end
+
+-- Check Money and Level
+while task.wait() do
+    pcall(function()
+        getgenv().money1 = game.Players.LocalPlayer.leaderstats['C$'].Value
+        getgenv().lv1 = game.Players.LocalPlayer.leaderstats.Level.Value
+
+        task.wait(wait_time)
+
+        getgenv().money2 = game.Players.LocalPlayer.leaderstats['C$'].Value
+        getgenv().lv2 = game.Players.LocalPlayer.leaderstats.Level.Value
+
+        if money1 == money2 then game:Shutdown() end
+        if lv1 == lv2 then game:Shutdown() end
+    end)
+end

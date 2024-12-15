62befee14063321ce801879450d3e5cf0c46d75c
Pxthanawin <96330997+Pxthanawin@users.noreply.github.com>
2024-12-15 19:58:07 +0700

    Update tsatf.lua

--- tsatf.lua
+++ tsatf.lua
@@ -20,11 +20,8 @@ local VirtualInputManager = game:GetService("VirtualInputManager")
 local GuiService = game:GetService("GuiService")
 local Workspace = game:GetService("Workspace")
 local Backpack = LocalPlayer.Backpack
-local playerName = game.Players.LocalPlayer.Name
-local userId = game.Players.LocalPlayer.UserId
 
 local rodNameCache = nil
-local webhookUrl = "https://discord.com/api/webhooks/1313075518727393310/qFe8ooPPvaJnbD1QbL3sYd3LZCVrqyVyheY47Wm7zwDlsPbKq2-llKLg6p48jD98ex4k"
 
 -- local oxygen = LocalPlayer.Character.client:FindFirstChild("oxygen")
 -- oxygen.Disabled = true
@@ -306,7 +303,7 @@ end)
 
 -- Monitor Money Changes
 task.spawn(function()
-    repeat task.wait() until getgenv().StartFarm
+    repeat task.wait() until getgenv().ScriptRunning
     task.wait(20)
     local countM = 0
     local money = LocalPlayer:FindFirstChild("leaderstats") and game.Players.LocalPlayer.leaderstats:FindFirstChild("C$") and LocalPlayer.leaderstats["C$"].Value or 0
@@ -324,7 +321,6 @@ task.spawn(function()
             end)
         end
         if countM >= 40 then
-            task.wait(3)
             game:Shutdown()
         end
     end
@@ -416,6 +412,42 @@ pcall(function()
         else
             warn("PhysicsService not available.")
         end
+
+        -- ลดการใช้งาน MeshParts และ Special Meshes
+        for _, v in pairs(workspace:GetDescendants()) do
+            if v:IsA("MeshPart") then
+                v.Material = Enum.Material.SmoothPlastic
+                v.TextureID = ""
+            elseif v:IsA("SpecialMesh") then
+                v.TextureId = ""
+            end
+        end
+
+        -- ปิดการใช้งานการสร้างวัตถุใหม่ที่ไม่จำเป็น
+        if hookfunction and setreadonly then
+            local mt = getrawmetatable(game)
+            local old = mt.__newindex
+            setreadonly(mt, false)
+            local sda
+            sda = hookfunction(old, function(t, k, v)
+                if k == "Material" then
+                    if v ~= Enum.Material.Neon and v ~= Enum.Material.Plastic and v ~= Enum.Material.ForceField then
+                        v = Enum.Material.Plastic
+                    end
+                elseif k == "TopSurface" or k == "BottomSurface" then
+                    v = "Smooth"
+                elseif k == "Reflectance" or k == "WaterWaveSize" or k == "WaterWaveSpeed" or k == "WaterReflectance" then
+                    v = 0
+                elseif k == "WaterTransparency" then
+                    v = 1
+                elseif k == "GlobalShadows" then
+                    v = false
+                end
+                return sda(t, k, v)
+            end)
+            setreadonly(mt, true)
+        end
+    end
 
     -- เรียกใช้ฟังก์ชัน
     OptimizeGamePerformance()

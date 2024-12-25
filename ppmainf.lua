c07e75f372aac1ee7d96d726ba48bae671ef9a60
Pxthanawin <96330997+Pxthanawin@users.noreply.github.com>
2024-12-25 19:33:21 +0700

    Update ppmainf.lua

--- ppmainf.lua
+++ ppmainf.lua
@@ -16,6 +16,7 @@ local Character = LocalPlayer.Character
 local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
 
 local StatsRod = ReplicatedStorage.playerstats[LocalPlayer.Name].Rods
+local rodNameCache = ReplicatedStorage.playerstats[LocalPlayer.Name].Stats.rod.Value
 
 local StartFarm
 
@@ -32,69 +33,9 @@ local ListRod = {
     ["DestinyRod"] = {"Destiny Rod", 190000}
 }
 
-local zoneList = function(ZoneName, LoadData)
-    if ZoneName == "The Depths" then
-        local DataZone = {
-            Pos = Vector3.new(841, -750, 1246),
-            Cam = CFrame.new(0.943815053, 141.073318, -0.428265214, -0.999930441, -0.0116165085, 0.00204831036, 0, 0.173648715, 0.98480773, -0.0117957117, 0.984739244, -0.173636645),
-            SellPos = Vector3.new(959.983887, -711.580261, 1262.43103),
-            SellCFrame = CFrame.new(959.983887, -711.580261, 1262.43103, -0.170621768, 1.00927435e-08, 0.985336602, 9.79065007e-08, 1, 6.71063694e-09, -0.985336602, 9.7615839e-08, -0.170621768),
-            SellCam = CFrame.new(947.130798, -711.47113, 1262.57898, -0.0641093925, -0.115132757, -0.991279244, 0, 0.99332267, -0.11537008, 0.997942924, -0.00739630591, -0.0636813045)
-        }
-        if LoadData then
-            DataZone.npc = workspace.world.npcs:WaitForChild("Milo Merchant", math.huge)
-            DataZone.npcpos = DataZone.npc.HumanoidRootPart.Position
-        end
-        return DataZone
-    end
-end
-
 local function extractNumber(String)
     return tonumber((String:gsub("[^%d]", "")))
 end
-
-
-local setZone = function(ZoneName)
-
-    if StartFarm then
-        return
-    end
-
-    Character = LocalPlayer.Character
-    HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
-
-    local zone = zoneList(ZoneName)
-    local bodyPosition = Instance.new("BodyPosition")
-    bodyPosition.Position = zone.SellPos
-    bodyPosition.MaxForce = Vector3.new(math.huge,math.huge, math.huge)
-    bodyPosition.Parent = HumanoidRootPart
-
-    zone = zoneList(ZoneName, true)
-
-    repeat task.wait() until (HumanoidRootPart.Position - zone.npcpos).Magnitude <= 6
-
-    local camera = workspace.Camera
-    camera.CameraType = Enum.CameraType.Scriptable
-    camera.CFrame = zone.SellCam
-    HumanoidRootPart.CFrame = zone.SellCFrame
-
-    local Highlight = zone.npc:WaitForChild("Highlight", 10)
-    local dialog = zone.npc:FindFirstChild("dialogprompt")
-
-    if Highlight then
-        if dialog then
-            dialog.HoldDuration = 0
-            dialog:InputHoldBegin()
-            dialog:InputHoldEnd()
-            bodyPosition:Destroy()
-            StartFarm = true
-        end
-    else
-        game:Shutdown()
-    end
-
-end
-
 
 local purchaseRod = function(RodName, Price)
     local money = extractNumber(LocalPlayer.leaderstats["C$"].Value)
@@ -121,89 +62,85 @@ task.spawn(function()
     end
 end)
 
+local equipRod = function(RodPriority)
+    for _, v in ipairs(RodPriority) do
+        if StatsRod:FindFirstChild(v) then
+            if rodNameCache ~= v then
+                Character.Humanoid:UnequipTools()
+                RunService.Heartbeat:Wait()
+                ReplicatedStorage.events.equiprod:FireServer(v)
+                RunService.Heartbeat:Wait()
+            end
+            return
+        end
+    end
+end
+
 
 -- Main auto Fish
 local autoFish = function()
-    local zone = "The Depths"
-    setZone(zone)
-    repeat task.wait() until StartFarm
 
-    zone = zoneList(zone, true)
+    local RodPriority = {
+        [1] = "Rod Of The Depths",
+        [2] = "Aurora Rod",
+        [3] = "Steady Rod"
+    }
 
-    local camera = workspace.Camera
-    camera.CameraType = Enum.CameraType.Scriptable
-    camera.CFrame = zone.Cam
-
-    local bodyPosition = Instance.new("BodyPosition")
-    bodyPosition.Position = zone.Pos
-    bodyPosition.MaxForce = Vector3.new(math.huge,math.huge, math.huge)
-    bodyPosition.Parent = HumanoidRootPart
-
-    repeat task.wait() until (HumanoidRootPart.Position - zone.Pos).Magnitude <= 1
-    task.wait(0.2)
+    Character.Torso.CFrame = CFrame.new(841, -750, 1246)
 
     Character.Torso.Anchored = true
     Character.Humanoid.Sit = true
 
     while config.AutoFish and RunService.Heartbeat:Wait() do
-        pcall(function()
-            local rodNameCache = ReplicatedStorage.playerstats[LocalPlayer.Name].Stats.rod.Value
 
-            if rodNameCache ~= "Steady Rod" then
-                if StatsRod:FindFirstChild("Steady Rod") then
-                    ReplicatedStorage:WaitForChild("events"):WaitForChild("equiprod"):FireServer("Steady Rod")
-                    LocalPlayer.Character.Humanoid:UnequipTools()
-                    return
-                end
+        rodNameCache = ReplicatedStorage.playerstats[LocalPlayer.Name].Stats.rod.Value
+
+        local rod = Backpack:FindFirstChild(rodNameCache) or (LocalPlayer.Character and LocalPlayer.Character:FindFirstChild(rodNameCache))
+
+        if not rod then
+            continue
+        end
+
+        if rod.Parent == Backpack then
+            Character.Humanoid:EquipTool(rod)
+            equipRod(RodPriority)
+            continue
+        end
+
+        if rod:FindFirstChild("bobber") then
+
+            local shakeUI = PlayerGui:WaitForChild("shakeui", 3)
+            if not shakeUI then
+                Character.Humanoid:UnequipTools()
+                continue
             end
 
-            local rod = Backpack:FindFirstChild(rodNameCache) or (LocalPlayer.Character and LocalPlayer.Character:FindFirstChild(rodNameCache))
-    
-            if not rod then
-                return
-            end
-    
-            if rod.Parent == Backpack then
-                Character.Humanoid:EquipTool(rod)
-            end
-    
-            if rod:FindFirstChild("bobber") then
-
-                local shakeUI = PlayerGui:WaitForChild("shakeui", 3)
-                if not shakeUI then
-                    Character.Humanoid:UnequipTools()
-                    return
+            while PlayerGui:FindFirstChild("shakeui") do
+                local button = shakeUI.safezone:FindFirstChild("button")
+                if button then
+                    button.Size = UDim2.new(1001, 0, 1001, 0)
+                    VirtualUser:Button1Down(Vector2.new(1, 1))
+                    VirtualUser:Button1Up(Vector2.new(1, 1))
                 end
-
-                while PlayerGui:FindFirstChild("shakeui") do
-                    local button = shakeUI.safezone:FindFirstChild("button")
-                    if button then
-                        button.Size = UDim2.new(1001, 0, 1001, 0)
-                        VirtualUser:Button1Down(Vector2.new(1, 1))
-                        VirtualUser:Button1Up(Vector2.new(1, 1))
-                    end
-                    RunService.Heartbeat:Wait()
-                end
-
-                repeat
-                    ReplicatedStorage.events.reelfinished:FireServer(100, true)
-                    RunService.Heartbeat:Wait()
-                until not rod.values.bite.Value
-
-                if config.AutoSell then
-                    zone.npc.merchant.sellall:InvokeServer()
-                end
-
-                LocalPlayer.Character.Humanoid:UnequipTools()
-
-            else
-                rod.events.cast:FireServer(100)
+                RunService.Heartbeat:Wait()
             end
-        end)
+
+            repeat
+                ReplicatedStorage.events.reelfinished:FireServer(100, true)
+                RunService.Heartbeat:Wait()
+            until not rod.values.bite.Value
+
+            if config.AutoSell then
+                ReplicatedStorage:WaitForChild("events"):WaitForChild("SellAll"):InvokeServer()
+            end
+
+            LocalPlayer.Character.Humanoid:UnequipTools()
+
+        else
+            rod.events.cast:FireServer(100)
+        end
+
     end
-
-    camera.CameraType = Enum.CameraType.Custom
-    bodyPosition:Destroy()
 end
 
 if config.AutoFish then

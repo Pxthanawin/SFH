af8b33678681a128afd5219b2ea79f22b776ab1f
Pxthanawin <96330997+Pxthanawin@users.noreply.github.com>
2025-01-15 01:56:29 +0700

    Update ppmainf.lua

--- ppmainf.lua
+++ ppmainf.lua
@@ -913,9 +913,10 @@ task.spawn(function()
             end]]
 
             if PlayerGui:FindFirstChild("reel") then
+                task.wait(0.2)
                 ReplicatedStorage.events.reelfinished:FireServer(100, true)
                 PlayerGui.reel:Destroy()
-                task.wait(0.3)
+                task.wait(0.2)
                 Character.Humanoid:UnequipTools()
             end
 

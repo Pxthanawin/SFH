d3019c7fdc351539b85c5ec508e7b84b71111939
Pxthanawin <96330997+Pxthanawin@users.noreply.github.com>
2025-01-24 11:44:43 +0700

    Update ppmainf.lua

--- ppmainf.lua
+++ ppmainf.lua
@@ -1053,30 +1053,9 @@ task.spawn(function()
             while PlayerGui:FindFirstChild("shakeui") do
                 local button = shakeUI:FindFirstChild("button")
                 if button then
-                    local buttonAbsolutePosition = button.AbsolutePosition
-                    local buttonAbsoluteSize = button.AbsoluteSize
-                    local buttonCenter = Vector2.new(
-                        buttonAbsolutePosition.X + (buttonAbsoluteSize.X / 2),
-                        buttonAbsolutePosition.Y + (buttonAbsoluteSize.Y / 2)
-                    )
-            
-                    VirtualInputManager:SendMouseButtonEvent(
-                        buttonCenter.X,     -- ตำแหน่ง X
-                        buttonCenter.Y,     -- ตำแหน่ง Y
-                        0,                  -- MouseButton ID (0 สำหรับคลิกซ้าย)
-                        true,               -- กดปุ่มลง
-                        button,             -- เป้าหมาย: button
-                        0            -- ID สำหรับคลิก
-                    )
-            
-                    VirtualInputManager:SendMouseButtonEvent(
-                        buttonCenter.X,
-                        buttonCenter.Y,
-                        0,
-                        false,              -- ปล่อยปุ่ม
-                        button,             -- เป้าหมาย: button
-                        0
-                    )
+                    GuiService.SelectedObject = button
+                    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, nil)
+                    VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, nil)
                 end
                 RunService.Heartbeat:Wait()
             end

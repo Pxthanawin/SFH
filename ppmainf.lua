3810de559b9b7301036ce5cbd3d97f21d0ac4353
Pxthanawin <96330997+Pxthanawin@users.noreply.github.com>
2025-02-15 05:58:02 +0700

    Update ppmainf.lua

--- ppmainf.lua
+++ ppmainf.lua
@@ -1141,12 +1141,18 @@ PlayerGui.ChildAdded:Connect(function(Child)
     if Child.Name == "shakeui" then
         Child:WaitForChild("safezone").ChildAdded:Connect(function(button)
             if button.Name == "button" then
-                GuiService.SelectedObject = button
-                GuiService.SelectedObject = button
-                VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, nil)
-                VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, nil)
-                VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, nil)
-                VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, nil)
+                button.Size = UDim2.new(1001, 0, 1001, 0)
+                while RunService.Heartbeat:Wait() do
+                    if button.IsLoaded then
+                        VirtualUser:Button1Down(Vector2.new(1, 1))
+                        VirtualUser:Button1Up(Vector2.new(1, 1))
+                        VirtualUser:Button1Down(Vector2.new(1, 1))
+                        VirtualUser:Button1Up(Vector2.new(1, 1))
+                        return
+                    elseif not button.Parent then
+                        return
+                    end
+                end
             else
                 RunService.Heartbeat:Wait()
                 button:Destroy()
